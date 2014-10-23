992daf9f16919f7ca6db3303b3850fa5dfb4b902
peterobbin <beijingwang@gmail.com>
2014-10-22 20:03:36 -0400

    changed value name

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -5,15 +5,18 @@ void setup()
 
 void loop()
 {
- int sensor, degrees;
+ int sensor, sensor2, degrees;
  sensor = analogRead(0);
+ sensor2 = analogRead(1);
  degrees = map(sensor, 768, 853, 400, 500);
  
 
  // Serial.print("[");
   Serial.print(degrees,DEC);
-  //Serial.print(",");
- // Serial.print(sensor,DEC);
+  Serial.print(",");
+  Serial.println(sensor2,DEC);
+  
+  
   //Serial.println("]");
   delay(100);
 
