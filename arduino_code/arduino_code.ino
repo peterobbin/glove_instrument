622739ec4a7a320387f75227fe9d1b67efaaef16
peterobbin <beijingwang@gmail.com>
2014-10-23 23:25:18 -0400

    Now work with 4 fingers
    Serial communication OK.

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -5,16 +5,28 @@ void setup()
 
 void loop()
 {
- int sensor, sensor2, degrees;
+ int sensor, sensor2, sensor3, sensor4, degrees, degrees2, degrees3, degrees4;
  sensor = analogRead(0);
  sensor2 = analogRead(1);
+ sensor3 = analogRead(2);
+ sensor4 = analogRead(3);
+ 
  degrees = map(sensor, 768, 853, 400, 500);
+ degrees2 = map(sensor2, 768, 853, 600, 700);
+ degrees3 = map(sensor3, 768, 853, 200, 300);
+ degrees4 = map(sensor4, 768, 853, 400, 800);
  
 
  // Serial.print("[");
   Serial.print(degrees,DEC);
   Serial.print(",");
-  Serial.println(sensor2,DEC);
+  Serial.print(degrees2,DEC);
+  Serial.print(",");
+  Serial.print(degrees3,DEC);
+  Serial.print(",");
+  Serial.print(degrees4,DEC);
+  Serial.println();
+  
   
   
   //Serial.println("]");
